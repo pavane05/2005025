d28b633de715957c2dd1cbb7fafc9f4a755bc2f5
hyunbin <hyunbin6987@gmail.com>
2023-11-23 09:54:33 +0900

    test

---
+++ src/main/java/com/example/gametalk_2005025/Gametalk2005025Application.java
@@ -0,0 +1,13 @@
+package com.example.gametalk_2005025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gametalk2005025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gametalk2005025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gametalk_2005025/Gametalk2005025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gametalk_2005025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gametalk2005025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
